dd77e644c46348bc58dab064addf901a14f02910
Egor <bishep.ok@yandex.ru>
2025-05-10 00:09:02 +0300

    implemented a logout function

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,24 +1,33 @@
 <template>
-    <nav class="navbar">
-        <h3>Online Bank</h3>
+  <nav class="navbar">
+    <h3>Online Bank</h3>
 
-        <ul class="navbar-menu">
-            <li>
-                <router-link to="/">Заявки</router-link>
-            </li>
-            <li>
-                <router-link to="/">Помощь</router-link>
-            </li>
-            <li>
-                <a href="#">Сообщение</a>
-            </li>
-            <li>
-                <a href="#">Выход</a>
-            </li>
-        </ul>
-    </nav>
+    <ul class="navbar-menu">
+      <li>
+        <router-link to="/">Заявки</router-link>
+      </li>
+      <li>
+        <router-link to="/">Помощь</router-link>
+      </li>
+      <li>
+        <a href="#">Сообщение</a>
+      </li>
+      <li>
+        <a href="#" @click.prevent="logout">Выход</a>
+      </li>
+    </ul>
+  </nav>
 </template>
 
-<script setup></script>
+<script setup>
+import { useRouter } from "vue-router";
+import { useStore } from "vuex";
 
-<style scoped></style>
+const router = useRouter();
+const store = useStore();
+
+const logout = () => {
+  store.commit("auth/logout");
+  router.push("/auth");
+};
+</script>
